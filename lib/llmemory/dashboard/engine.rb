afc931e8f217d1da74f4d5dc70306e8e3e7b49d7
jmdiez <demonodojo@gmail.com>
2026-02-02 13:34:42 +0100

    expose routes

--- lib/llmemory/dashboard/engine.rb
+++ lib/llmemory/dashboard/engine.rb
@@ -4,6 +4,8 @@ module Llmemory
   module Dashboard
     class Engine < ::Rails::Engine
       isolate_namespace Llmemory::Dashboard
+
+      config.root = File.expand_path("..", __FILE__)
     end
   end
 end
